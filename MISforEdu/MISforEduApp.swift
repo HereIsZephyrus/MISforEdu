e7f1d4ed98856b3fde03cb7aa63dcfd05deace0a
ChanningTong <Channing_TongCN@outlook.com>
2024-01-16 13:40:00 +0800

    Initial Commit

---
+++ MISforEdu/MISforEduApp.swift
@@ -0,0 +1,17 @@
+//
+//  MISforEduApp.swift
+//  MISforEdu
+//
+//  Created by ChanningTong on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MISforEduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
